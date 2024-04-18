5db668bd63d3c07fafdbaf1a47560ef86b344475
gwahyun <60453182+gwahyun@users.noreply.github.com>
2024-04-18 15:11:14 +0900

    Refactor: 로더를 개별 컴포넌트로 분리

---
+++ remember416/src/Loader.tsx
@@ -0,0 +1,11 @@
+function Loader() {
+  return (
+    <div className="flex flex-col justify-center items-center h-full">
+      <h5 className="spin text-xs">세월호 10주기 추모주간</h5>
+      <span className="spin text-[8rem]">🎗️</span>
+      <h5 className="spin text-xs">기억을 불러오는 중</h5>
+    </div>
+  );
+}
+
+export default Loader;
